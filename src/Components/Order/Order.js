75efe3ab0c82c5211f314cce7b55992c339e2894
Dayal Sharan Saini <dayalsaini67@gmail.com>
2018-02-08 16:49:17 +0530

    git try commit

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Order.css';
 const order = (props) => {
     const ingredients= [];
-
+//
     for( let ingredientName in props.ingredients){
         ingredients.push(
             {
